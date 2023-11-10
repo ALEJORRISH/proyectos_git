180c2336d66538b708bfea9f24d67de2cff5659a
Diego Alejandro Buitrago Rivera <83962513+ALEJORRISH@users.noreply.github.com>
2023-11-09 21:11:40 -0500

    java apis

---
+++ Nueva carpeta/java/java.js
@@ -0,0 +1,23 @@
+let datos;
+
+async function verInfo(){
+    try{
+        const respuesta = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita");
+
+        if(respuesta.status==200){
+            datos = await respuesta.json();
+            console.log(datos);
+            document.getElementById("img1").src=datos.drinks[0].strDrinkThumb;
+            document.getElementById("nom1").innerHTML=datos.drinks[0].strInstructions;
+            document.getElementById("img2").src=datos.drinks[1].strDrinkThumb;
+            document.getElementById("nom2").innerHTML=datos.drinks[1].strInstructions;
+        }
+else{
+    alert("Datos no encontrados, intentelo de nuevo");
+}
+
+    } 
+    catch (error){
+    console.log(error);
+    }
+}
